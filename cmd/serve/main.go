111ce04b1187954eae10df815acb25a0229c89b0
bartmika <bart@mikasoftware.com>
2021-01-30 02:05:57 -0500

    Added graceful shutdown.

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -2,8 +2,14 @@
 package main
 
 import (
+    "context"
     "fmt"
+    "log"
     "net/http"
+    "os"
+    "os/signal"
+	"syscall"
+	"time"
 
     sqldb "github.com/bartmika/mulberry-server/pkg/db"
     "github.com/bartmika/mulberry-server/internal/repositories"
@@ -20,12 +26,44 @@ func main() {
     router := http.NewServeMux()
     router.HandleFunc("/", c.HandleRequests)
 
-	s := &http.Server{
+	srv := &http.Server{
 		Addr: fmt.Sprintf("%s:%s", "localhost", "5000"),
         Handler: router,
 	}
 
-    if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-        panic(err)
+    done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+    go runMainRuntimeLoop(srv)
+
+	log.Print("Server Started")
+
+	// Run the main loop blocking code.
+	<-done
+
+    stopMainRuntimeLoop(srv)
+}
+
+func runMainRuntimeLoop(srv *http.Server) {
+    if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+        log.Fatalf("listen: %s\n", err)
     }
+}
+
+func stopMainRuntimeLoop(srv *http.Server) {
+    log.Printf("Starting graceful shutdown now...")
+
+    // Execute the graceful shutdown sub-routine which will terminate any
+	// active connections and reject any new connections.
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+    log.Printf("Graceful shutdown finished.")
+    log.Print("Server Exited")
 }
